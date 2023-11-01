1eb983859e36b3108ba64b1a35c93e28a58075af
blackpilotlds <59564988+blackpilotlds@users.noreply.github.com>
2023-11-01 17:30:07 -0600

    files to work with

---
+++ jenkins_pipelines/02_infra_destroy_node-js_app.groovy
@@ -0,0 +1,39 @@
+pipeline {
+    agent any
+    tools {
+        terraform 'tf1.6'
+    }
+    stages {
+        stage('Clone Git repo') {
+            steps {
+                git(
+                    branch: 'main',
+                    url: 'https://github.com/blackpilotlds/PASV.git',
+                    credentialsId: 'access_to_git'
+                )
+            }
+        }
+        stage('Terraform Initialize and Plan Destroy') {
+            steps {
+                sh '''
+                cd "./2023-10-29 Sun #20/My Own/terraform_ansible_generic_instace_setup_template"
+                terraform init
+                terraform plan -destroy -out=destroyplan.tfplan
+                '''
+            }
+        }
+        stage('Plan verification and user input for Destroy') {
+            steps {
+                input message: 'proceed or abort?', ok: 'ok'
+            }
+        }
+        stage('Terraform Apply Destroy') {
+            steps {
+                sh '''
+                cd "./2023-10-29 Sun #20/My Own/terraform_ansible_generic_instace_setup_template"
+                terraform apply destroyplan.tfplan
+                '''
+            }
+        }
+    }
+}
